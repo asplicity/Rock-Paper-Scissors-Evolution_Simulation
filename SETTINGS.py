14a6def750ebb9a42969441a89d6e6e229b788fa
Tico <ticobeier@gmail.com>
2024-10-31 16:32:08 +0100

    did 200 days

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,7 +1,7 @@
-Start_people = 10_000
+Start_people = 500_000
 Mutation_factor = 0.2
-Tree_count = 50_000
-Days = 100
+Tree_count = 2_500_000
+Days = 200
 path_to_data = "data/table.csv"
 
 ROCK = 0
